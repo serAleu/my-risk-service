ed8cfe329418c4b603130a2f1727e705cb74fede
gleb.syrov <gleb.syrov@atmonline.asia>
2023-08-23 11:05:28 +0300

    fix aws config

--- src/main/java/asia/atmonline/myriskservice/config/AwsConfig.java
+++ src/main/java/asia/atmonline/myriskservice/config/AwsConfig.java
@@ -1,65 +1,125 @@
 package asia.atmonline.myriskservice.config;
 
-import asia.atmonline.myriskservice.data.entity.risk.responses.RiskResponseJpaEntity;
-import io.awspring.cloud.autoconfigure.sqs.SqsProperties.Listener;
-import io.awspring.cloud.sqs.config.SqsBootstrapConfiguration;
-import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.listener.acknowledgement.AcknowledgementOrdering;
-import io.awspring.cloud.sqs.listener.acknowledgement.handler.AcknowledgementMode;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
-import io.awspring.cloud.sqs.operations.TemplateAcknowledgementMode;
-import io.awspring.cloud.sqs.support.converter.SqsMessagingMessageConverter;
-import java.time.Duration;
-import org.jetbrains.annotations.NotNull;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory;
+import io.awspring.cloud.messaging.listener.QueueMessageHandler;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.core.env.ResourceIdResolver;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Import(SqsBootstrapConfiguration.class)
 @Configuration
+//@Getter
+@EnableSqs
 public class AwsConfig {
 
-  @NotNull
-  private static SqsMessagingMessageConverter getSqsMessagingMessageConverter() {
-    SqsMessagingMessageConverter messageConverter = new SqsMessagingMessageConverter();
-    MappingJackson2MessageConverter payloadConverter = new MappingJackson2MessageConverter();
-    payloadConverter.setPrettyPrint(true);
-    messageConverter.setPayloadMessageConverter(payloadConverter);
-    return messageConverter;
-  }
+  @Value("${aws.credentials.access-key}")
+  private String awsCredentialsAccessKey;
+  @Value("${aws.credentials.secret-key}")
+  private String awsCredentialsSecretKey;
+  @Value("${aws.default-task-executor.core-pool-size}")
+  private Integer awsDefaultTaskExecutorCorePoolSize;
+  @Value("${aws.default-task-executor.max-pool-size}")
+  private Integer awsDefaultTaskExecutorMaxPoolSize;
+  @Value("${aws.default-task-executor.queue-capacity}")
+  private Integer awsDefaultTaskExecutorQueueCapacity;
+  //  @Value("{$aws.default-task-executor.blocking-task-timeout}")
+//  private Long awsDefaultTaskExecutorBlockingTaskTimeout;
+  @Value("${aws.sqs.max-number-of-messages}")
+  private Integer awsSqsMaxNumberOfMessages;
+//  @Value("{$aws.sqs.back-off-time}")
+//  private String awsSqsBackOffTime;
 
   @Bean
-  public SqsTemplate template(SqsAsyncClient sqsAsyncClient) {
-    return SqsTemplate.builder()
-        .sqsAsyncClient(sqsAsyncClient)
-        .configure(options -> options
-            .acknowledgementMode(TemplateAcknowledgementMode.MANUAL)
-            .defaultPayloadClass(RiskResponseJpaEntity.class)
-        )
+  @Primary
+  public AmazonSQSAsync awsSQSAsync() {
+    return AmazonSQSAsyncClientBuilder.standard()
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+        .withRegion(Regions.AP_SOUTHEAST_1)
         .build();
   }
 
   @Bean
-  public Listener listener() {
-    return new Listener();
+  public BasicAWSCredentials awsCredentialsProvider() {
+    return new BasicAWSCredentials(awsCredentialsAccessKey, awsCredentialsSecretKey);
+  }
+
+//  @Bean
+//  public SimpleMessageListenerContainer simpleMessageListenerContainer() {
+//    SimpleMessageListenerContainer messageListenerContainer = simpleMessageListenerContainerFactory().createSimpleMessageListenerContainer();
+//    messageListenerContainer.setMessageHandler(queueMessageHandler());
+//    return messageListenerContainer;
+//  }
+
+  @Bean
+  public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync, MessageConverter messageConverter) {
+    return new QueueMessagingTemplate(amazonSQSAsync, (ResourceIdResolver) null, messageConverter);
+  }
+
+  @Bean(name = "threadPoolQueue")
+  public AsyncTaskExecutor createDefaultTaskExecutor() {
+    ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+    threadPoolTaskExecutor.setThreadNamePrefix("SQSExecutor - ");
+    threadPoolTaskExecutor.setCorePoolSize(awsDefaultTaskExecutorCorePoolSize);
+    threadPoolTaskExecutor.setMaxPoolSize(awsDefaultTaskExecutorMaxPoolSize);
+    threadPoolTaskExecutor.setQueueCapacity(awsDefaultTaskExecutorQueueCapacity);
+    threadPoolTaskExecutor.afterPropertiesSet();
+    threadPoolTaskExecutor.setRejectedExecutionHandler(new BlockingTaskSubmissionPolicy(1000));
+    return threadPoolTaskExecutor;
   }
 
   @Bean
-  SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
-    return SqsMessageListenerContainerFactory
-        .builder()
-        .configure(options -> options
-            .acknowledgementMode(AcknowledgementMode.ALWAYS)
-            .acknowledgementInterval(Duration.ofSeconds(3))
-            .acknowledgementThreshold(5)
-            .acknowledgementOrdering(AcknowledgementOrdering.ORDERED)
-            .messageConverter(getSqsMessagingMessageConverter())
-        )
-        .sqsAsyncClient(sqsAsyncClient)
-        .build();
+  public QueueMessageHandler queueMessageHandler() {
+    QueueMessageHandlerFactory queueMsgHandlerFactory = new QueueMessageHandlerFactory();
+    queueMsgHandlerFactory.setAmazonSqs(awsSQSAsync());
+    QueueMessageHandler queueMessageHandler = queueMsgHandlerFactory.createQueueMessageHandler();
+    List<HandlerMethodArgumentResolver> list = new ArrayList<>();
+    HandlerMethodArgumentResolver resolver = new PayloadMethodArgumentResolver(new MappingJackson2MessageConverter());
+    list.add(resolver);
+    queueMessageHandler.setArgumentResolvers(list);
+    return queueMessageHandler;
   }
 
+  @Bean
+  public QueueMessageHandlerFactory queueMessageHandlerFactory(
+      final AmazonSQSAsync amazonSQSAsync,
+      final MessageConverter messageConverter,
+      final ObjectMapper mapper) {
 
+    final QueueMessageHandlerFactory queueHandlerFactory = new QueueMessageHandlerFactory();
+    queueHandlerFactory.setAmazonSqs(amazonSQSAsync);
+    queueHandlerFactory.setArgumentResolvers(Collections.singletonList(new PayloadMethodArgumentResolver(messageConverter)));
+    queueHandlerFactory.setObjectMapper(mapper);
+    queueHandlerFactory.setMessageConverters(Collections.singletonList(messageConverter));
+    return queueHandlerFactory;
+  }
+
+  @Bean
+  protected MessageConverter messageConverter(ObjectMapper objectMapper) {
+
+    var converter = new MappingJackson2MessageConverter();
+    converter.setObjectMapper(objectMapper);
+    // Serialization support:
+    converter.setSerializedPayloadClass(String.class);
+    // Deserialization support: (suppress "contentType=application/json" header requirement)
+    converter.setStrictContentTypeMatch(false);
+    return converter;
+  }
 }
