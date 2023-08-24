5c0f0c4ca3edbdd4cdf5d1fd011f18ba1af6fd2c
gleb.syrov <gleb.syrov@atmonline.asia>
2023-08-24 15:29:23 +0300

    fix aws config

--- src/main/java/asia/atmonline/myriskservice/producers/BaseSqsProducer.java
+++ src/main/java/asia/atmonline/myriskservice/producers/BaseSqsProducer.java
@@ -1,12 +1,15 @@
 package asia.atmonline.myriskservice.producers;
 
 import asia.atmonline.myriskservice.data.entity.risk.responses.RiskResponseJpaEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.operations.SendResult;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +18,7 @@ import org.springframework.stereotype.Component;
 @Getter
 public abstract class BaseSqsProducer {
 
-  public static final String HEADER_CONTENT_TYPE_VALUE = "application/json;charset=utf-8";
+  public static final String HEADER_CONTENT_TYPE_VALUE = "application/json;charset=UTF-8";
   public static final String HEADER_CONTENT_TYPE = "contentType";
 
   private final SqsTemplate template;
@@ -27,10 +30,11 @@ public abstract class BaseSqsProducer {
 
   public void sendResponseToQueue(RiskResponseJpaEntity<? extends BaseSqsProducer> riskResponseJpaEntity, String queueName) {
     try {
-      SendResult<String> result = template.send(to -> to.queue(queueName)
-          .payload(riskResponseJpaEntity.toString())
-          .header(HEADER_CONTENT_TYPE, HEADER_CONTENT_TYPE_VALUE)
-      );
+      Message<?> msg = MessageBuilder.withPayload(riskResponseJpaEntity)
+          .setHeader(HEADER_CONTENT_TYPE, HEADER_CONTENT_TYPE_VALUE)
+          .build();
+
+      SendResult<?> result = template.send(queueName, msg);
       log.info(result.toString());
     } catch (Exception e) {
       log.error("my-risk-service-" + activeProfile + " Error while placing message to the " + queueName + " queue. " + e.getMessage());
